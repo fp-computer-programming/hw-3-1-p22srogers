3896ad2bbf4c839e98b09674face9c0d9d257a5c
P22srogers <p22srogers@fairfieldprep.org>
2021-09-28 10:19:55 -0400

    WIP

---
+++ hw3-1-1.py
@@ -0,0 +1,11 @@
+# Author: SMR (AMDG) 09/28/21
+team_1_wins=input("How many wins did team one have? ")
+team_1_ties=input("How many ties did team one have? ")
+team_2_wins=input("How many wins did team two have? ")
+team_2_ties=input("How many ties did team two have? ")
+wins_1=int(3*team_1_wins)
+ties_1=int(team_1_ties)
+wins_2=int(3*team_2_wins)
+ties_2=int(team_2_ties)
+total_1= wins_1 + ties_1
+total_2= wins_2+ties_2
